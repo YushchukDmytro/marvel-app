7be8ee8c7dc4b685e82a988d86fb5ae51929766e
Dima <Dreuk03@gmail.com>
2023-02-02 23:11:47 +0200

    Lesson 152.Error Boundaries

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,8 +4,10 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png'
+
 
 class App extends Component {
 
@@ -24,10 +26,16 @@ class App extends Component {
 			<div className='app'>
 				<AppHeader/>
 				<main>
-					<RandomChar/>
+					<ErrorBoundary>
+						<RandomChar/>
+					</ErrorBoundary>
 					<div className="char__content">
-						<CharList onSelectedChar = {this.onSelectedChar}/>
-						<CharInfo charId={this.state.selectedChar}/>
+						<ErrorBoundary>
+							<CharList onSelectedChar = {this.onSelectedChar}/>
+						</ErrorBoundary>
+						<ErrorBoundary>
+							<CharInfo charId={this.state.selectedChar}/>
+						</ErrorBoundary>
 					</div>
 					<img src={decoration} alt="vision" className="bg-decoration" />
 				</main>
--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -40,6 +40,7 @@ class CharInfo extends Component {
 			.getCharacter(charId)
 			.then(this.onCharLoaded)
 			.catch(this.onError)
+
 	}
 
 	onCharLoaded = (char) => {
